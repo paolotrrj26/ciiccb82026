67e83ae131314f72f5711edf52f3220d665d3b5a
Juan Paolo Tarroja <16576312+paolotrrj26@users.noreply.github.com>
2026-07-20 10:08:04 +0800

    Reference Lesson

---
+++ src/chapter1/examples/ReferenceLesson.java
@@ -0,0 +1,11 @@
+package chapter1.examples;
+
+public class ReferenceLesson {
+
+    public static void main(String[] args) {
+        java.util.Date today = new java.util.Date();
+        String greeting = "How are you?";
+
+        System.out.println(greeting);
+    }
+}
